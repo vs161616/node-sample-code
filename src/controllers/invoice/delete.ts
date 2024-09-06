469b105bf68356b1711b96bb20a5954068a2d4c6
vicky <vs161616@gmail.com>
2024-09-06 17:29:25 +0530

    changes in delete file

--- src/controllers/invoice/delete.ts
+++ src/controllers/invoice/delete.ts
@@ -12,6 +12,7 @@ const deleteWrapper: RequestHandler = async (req, res) => {
   const { id } = req.params;
 console.log("object")
 console.log("object")
+console.log("object")
 
   // Validate if the provided ID is a valid MongoDB ObjectId
   if (!isValidObjectId(id)) {
